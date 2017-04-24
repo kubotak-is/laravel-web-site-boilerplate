873ddea993e99fd4fbbf8281e5649d71512a13f8
kubotak-is <kubotak@istyle.co.jp>
2017-04-25 01:13:48 +0900

    modify: model

--- config/services.php
+++ config/services.php
@@ -30,7 +30,7 @@ return [
     ],
 
     'stripe' => [
-        'model' => App\User::class,
+        'model' => App\Authentication\Web::class,
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
